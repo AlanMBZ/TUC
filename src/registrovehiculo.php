6411834028417200b51e4a49ce5a3440f5500e9c
AlanMBZ <alan200470@hotmail.com>
2025-12-14 21:11:18 -0600

    Arreglado de botones laterales en pantalla de registro de vehiculo

--- src/registrovehiculo.php
+++ src/registrovehiculo.php
@@ -12,29 +12,59 @@
 <label for="menu-toggle" class="toggle-btn">☰</label>
 
 <aside class="sidebar">
-    <label for="menu-toggle" class="close-btn">×</label>
-    <div class="user-info">
-        <img src="https://cdn-icons-png.flaticon.com/512/552/552721.png">
-        <div class="user-text">
-            <span class="user-name">Usuario</span>
-            <span class="user-role">No conectado</span>
+        <label for="menu-toggle" class="close-btn">×</label>
+
+        <div class="user-info">
+            <img src="https://cdn-icons-png.flaticon.com/512/552/552721.png">
+            <div class="user-text">
+                <span class="user-name">Usuario</span>
+                <span class="user-role">No conectado</span>
+            </div>
         </div>
-    </div>
-    <div class="menu-links">
-        <a href="#inicio">
-            <img src="https://marketplace.canva.com/eGqLY/MAGPH-eGqLY/1/tl/canva-round-house-icon-MAGPH-eGqLY.png">
-            <span onclick="inicio()">Inicio</span>
-        </a>
-        <a href="#perfil">
-            <img src="https://cdn-icons-png.flaticon.com/512/848/848006.png">
-            <span>Perfil</span>
-        </a>
-        <a href="#vehiculos">
-            <img src="https://www.nicepng.com/png/full/89-891434_white-car-icon-png-car-icon-green-png.png">
-            <span onclick="vehiculo()">Vehículos</span>
-        </a>
-    </div>
-</aside>
+
+        <div class="menu-links">
+            <a href="#inicio">
+                <img src="https://marketplace.canva.com/eGqLY/MAGPH-eGqLY/1/tl/canva-round-house-icon-MAGPH-eGqLY.png">
+                <span onclick="inicio()">Inicio</span>
+            </a>
+
+            <a href="#perfil">
+                <img src="https://cdn-icons-png.flaticon.com/512/848/848006.png">
+                <span>Perfil</span>
+            </a>
+
+            <a href="#vehiculos">
+                <img src="https://www.nicepng.com/png/full/89-891434_white-car-icon-png-car-icon-green-png.png">
+                <span onclick="vehiculo()">Vehículos</span>
+            </a>
+
+            <a href="#viaje">
+                <img src="https://cdn-icons-png.flaticon.com/512/3731/3731420.png">
+                <span>Viaje</span>
+            </a>
+
+            <a href="#ruta">
+                <img src="https://cdn-icons-png.flaticon.com/512/599/599129.png">
+                <span>Rutas</span>
+            </a>
+
+            <a href="#ajustes">
+                <img src="https://cdn-icons-png.flaticon.com/512/456/456167.png">
+                <span>Ajustes</span>
+            </a>
+
+            <a href="#ayuda">
+                <img src="https://cdn.iconscout.com/icon/free/png-256/free-ayuda-icon-svg-download-png-2598199.png">
+                <span>Ayuda</span>
+            </a>
+        </div>
+        <div class="menu-links">
+            <a href="#salir" class="logout">
+                <img src="https://cdn-icons-png.flaticon.com/512/16385/16385164.png">
+                <span>Salir</span>
+            </a>
+        </div>
+    </aside>
 
     <div class="main">
     <div class="card">
