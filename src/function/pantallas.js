fbb802be67b0f2a8d73f3547175f74db1a52495f
AlanMBZ <alan200470@hotmail.com>
2025-12-29 21:03:34 -0600

    JS pantallas completo

--- src/function/pantallas.js
+++ src/function/pantallas.js
@@ -2,7 +2,7 @@ function vehiculo(){
     window.open("vehiculo.php");
 }
 function inicio(){
-    window.open("inicio.php");
+    window.open("inicioconductor.php");
 }
 function login(){
     window.open("login.php");
@@ -11,13 +11,13 @@ function Ruta(){
     window.open("rutas.php");
 }
 function Formularioruta(){
-window.open("formularioruta.php");
+    window.open("formularioruta.php");
 }
 function modificar(){
     window.open("modificarruta.php");
 }
 function viajes(){
-        window.open("viajes.php");
+    window.open("viajes.php");
 }
 function perfil(){
     window.open("perfil.php");
